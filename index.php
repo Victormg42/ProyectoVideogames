07fd4820bb1a6ccbb8676fc162c7346291f652b8
Victormg42 <100005779.joan23@fje.edu>
2020-10-14 15:34:02 +0200

    Primera subida del proyecto

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Proyecto Inicial</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    </head>
+
+<body>
+<div class="header">
+    <div class="topnav">
+            <a href="index.php" style="margin-left: 25%;"><strong>Inicio</a>
+            <a href="#">Plataformas</a>
+            <a href="#">Noticias</a>
+            <a href="#">Galeria</strong></a>
+                <div class="idioma">
+                    <select class="selectpicker" data-width="fit">
+                        <option>Idioma</option>
+                        <option data-content='<span class="flag-icon flag-icon-us"></span> English'>English</option>
+                        <option data-content='<span class="flag-icon flag-icon-mx"></span> Español'>Español</option>
+                        <input type="submit" value="Enviar">
+                </div>
+    </div>
+    <h1>NOTICIAS DE VIDEOJUEGOS</h1>        
+</div>
+
+<div class="contador">
+<p></p>
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="inici">
+        <img src="./img/imagen1.jpg">
+    </div>
+    <h2>Bienvenidos</h2>
+    <p>En esta pagina, encontraremos noticias sobre los distintos videojuegos que estan en el mercado actualmente, asi como tambien sobre las distintas videoconsolas actuales. Dentro del apartado de "Plataformas", encontraremos una seccion de todas las consolas de actual generacion, junto con una lista y descripcion de sus videojuegos mas populares hasta la fecha.</p>
+</div>
+
+    <div class="clasificaciones">
+    <h2>CLASIFICACIONES</h2>
+    </div>
+<div class="column">
+    <h3>NOTICIAS<h3>
+    <div class="noticias">
+        <img src="./img/noticias1.jpg">
+    </div>
+    <div class="texto1">3djuegos</div>
+    <div class="noticias">
+        <img src="./img/noticias2.jpg">
+    </div>
+    <div class="texto2">Portal IGN</div>
+    <div class="noticias">
+        <img src="./img/noticias3.jpg">
+    <div class="texto3">Noticias Playstation</div>
+    </div>
+</div>
+</div>
+<footer>
+    <h3><strong>Contacte</strong></h3>
+    <p>11 de octubre del 2020</p>
+    <p>Jesuites Bellvitge Joan XXIII</p>
+    <p>Hecho por: Víctor Martínez García</p>
+</footer>
+</body>
+</html>
